2e72823b028626af16ffbc436c079be3c572485b
Noah Salaway <nms348@nyu.edu>
2016-04-19 19:46:27 -0400

    maze prototype

---
+++ Assets/Scripts/MazeLogic.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeLogic : MonoBehaviour {
+
+    public int modifierOne;
+    public int modifierTwo;
+    public int modifierThree;
+
+    int randX = 1, randZ;
+
+    public GameObject player;
+    public GameObject goal;
+
+    public GameObject[] leftMazes = new GameObject[3];
+    public GameObject[] rightMazes = new GameObject[3];
+
+    // Use this for initialization
+    void Start () {
+        CreateMaze();
+        CreateUnits();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+
+    void CreateMaze()
+    {
+        GameObject myLeftMaze = (GameObject)Instantiate(leftMazes[modifierOne], transform.position, Quaternion.identity);
+        GameObject myRightMaze = (GameObject)Instantiate(rightMazes[modifierTwo], transform.position, Quaternion.identity);
+    }
+
+    void CreateUnits()
+    {
+        randX = Random.Range(-4, 6);
+        randZ = Random.Range(-4, 6);
+
+        Vector3 playerStartLocation = new Vector3(randX, 1, randZ);
+        GameObject myPlayer = (GameObject)Instantiate(player, playerStartLocation, Quaternion.identity);
+
+
+        randZ = Random.Range(-4, 6);
+        if (randX < 0.5f)
+        {
+            randX = Random.Range(2, 6);
+
+        }
+        else
+        {
+            randX = Random.Range(-4, 1);
+        }
+
+        Vector3 goalLocation = new Vector3(randX, 1, randZ);
+        GameObject myGoal = (GameObject)Instantiate(goal, goalLocation, Quaternion.identity);
+    }
+}
