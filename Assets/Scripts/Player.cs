0fb91e14f3cdfa194297d2d2eb43a0f6772e5d57
Noah Salaway <nms348@nyu.edu>
2016-04-27 17:46:45 -0400

    added some sounds to maze and changed arrow location to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
     int counter = 0;
+    public AudioSource[] goodSounds = new AudioSource[4];
+    public AudioSource tick;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +20,6 @@ public class Player : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0))
         {
-
             if (Physics.Raycast(mouseRay, out mouseRayHit, 1000f))
             {
 
@@ -27,6 +28,7 @@ public class Player : MonoBehaviour {
                 switch (mouseRayHit.collider.tag)
                 {
                     case "Up":
+                        tick.Play();
                         Ray upRay = new Ray(transform.position, Vector3.forward);
                         RaycastHit upRayInfo;
                         if (Physics.Raycast(upRay, out upRayInfo, 1))
@@ -49,11 +51,13 @@ public class Player : MonoBehaviour {
                         else
                         {
                             transform.position = transform.position + Vector3.forward;
+                            GoodSound();
                         }
                         break;
 
 
                     case "Down":
+                        tick.Play();
                         Ray downRay = new Ray(transform.position, Vector3.back);
                         RaycastHit downRayInfo;
                         if (Physics.Raycast(downRay, out downRayInfo, 1))
@@ -77,11 +81,13 @@ public class Player : MonoBehaviour {
                         else
                         {
                             transform.position = transform.position + Vector3.back;
+                            GoodSound();
                         }
                         break;
 
 
                     case "Left":
+                        tick.Play();
                         Ray leftRay = new Ray(transform.position, Vector3.left);
                         RaycastHit leftRayInfo;
                         if (Physics.Raycast(leftRay, out leftRayInfo, 1))
@@ -105,11 +111,13 @@ public class Player : MonoBehaviour {
                         else
                         {
                             transform.position = transform.position + Vector3.left;
+                            GoodSound();
                         }
                         break;
 
 
                     case "Right":
+                        tick.Play();
                         Ray rightRay = new Ray(transform.position, Vector3.right);
                         RaycastHit rightRayInfo;
                         if (Physics.Raycast(rightRay, out rightRayInfo, 1))
@@ -133,6 +141,7 @@ public class Player : MonoBehaviour {
                         else
                         {
                             transform.position = transform.position + Vector3.right;
+                            GoodSound();
                         }
                         break;
                 }
@@ -140,4 +149,16 @@ public class Player : MonoBehaviour {
         }
 
     }
+
+    void GoodSound()
+    {
+        int randomizer = Random.Range(0, 2);
+        if (randomizer == 0)
+        {
+            randomizer = Random.Range(0, 4);
+            goodSounds[randomizer].Play();
+        }
+    }
+
+
 }
