b160ac8428d7b817006b949ebe7196c07f64548c
Bria Smith <bs2610@nyu.edu>
2016-05-11 13:21:57 -0400

    All reactions for the first two eyes (sending them now so that you guys have something to work with).
    Check code for: First_Negative, Second_Negative, RobotReactions (where the first positive reactions are, I started working in this before I thought about how  I would split everything up) and Second_Positive.
    Some things have been commented out for testing reasons, but they should be labeled in the code.
    
    All assests are public.

--- Assets/Scripts/Robot_Reaction.cs
+++ Assets/Scripts/Robot_Reaction.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class Robot_Reaction : MonoBehaviour {
 
-	//the original eyes vs the new eyes. 
 
-	public GameObject eye1Normal;
-	public GameObject eye1Good;
+	//****THE FOLLOWING ARE PUBLIC OBJECTS FOR EYE 0 (hearts)****
+	//turned off for testing
+//	public GameObject closedRight;
+//	public GameObject openRight;
 
 
 	//materials
@@ -34,21 +35,54 @@ public class Robot_Reaction : MonoBehaviour {
 	public GameObject heartLeft;
 	public GameObject heartRight;
 
+	//****END OBJECTS FOR EYE 0.****
+
+
+
+	//****THE FOLLOWING ARE PUBLIC OBJECTS FOR EYE 1 (visor)****
+
+	public GameObject visor;
+	public Material winkyFace;
+	public Material goodReaction;
+
+	//****END OBJECTS FOR EYE 1.
+
+
+
+
+
 
 	// Use this for initialization
 	void Start () {
 
-	
 
-		//this replaces the normal winking eyes with two open eyes and starts a coroutine. 
-		foreach (MeshRenderer eye1Renderer in eye1Normal.GetComponentsInChildren<MeshRenderer>()) {
-			eye1Renderer.enabled = false;
-		}
+		//these are objects that should be invisible. 
+		//commented out for testing purposes. 
+//		foreach (MeshRenderer openRightRenderer in openRight.GetComponentsInChildren<MeshRenderer>()) {
+//			openRightRenderer.enabled = false;
+//		}
+//
+		//****IF EYE 0 IS CHOSEN:****
 
-		foreach (MeshRenderer eye2Renderer in eye1Good.GetComponentsInChildren<MeshRenderer>()) {
-			eye2Renderer.enabled = true;
-		}
+		//this replaces the closed right eye with an open right eye. 
+		//also turned OFF for testing reasons
 
+//		foreach (MeshRenderer closedLeftRenderer in closedLeft.GetComponentsInChildren<MeshRenderer>()) {
+//			closedLeftRenderer.enabled = false;
+//		}
+//
+//		foreach (MeshRenderer openLeftRenderer in openLeft.GetComponentsInChildren<MeshRenderer>()) {
+//			openLeftRenderer.enabled = true;
+//		}
+//
+//		//this replaces the closed right eye with an open right eye. 
+//		foreach (MeshRenderer openRightRenderer in openRight.GetComponentsInChildren<MeshRenderer>()) {
+//			openRightRenderer.enabled = true;
+//		}
+//
+//		foreach (MeshRenderer closedRightRenderer in closedRight.GetComponentsInChildren<MeshRenderer>()) {
+//			closedRightRenderer.enabled = false;
+//		}
 
 
 		//this coroutine is in charge of the FIRST POSITIVE REACTION for EYE 0.
@@ -72,41 +106,30 @@ public class Robot_Reaction : MonoBehaviour {
 
 		StartCoroutine (HappyLightDance1 ());
 
+		//****end IF EYE 0 IS CHOSEN
 
 
 
+		//****IF EYE 1 IS CHOSEN****
 
 
+		visor.GetComponent<MeshRenderer> ().material = goodReaction;
+		visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (0.02f, -0.12f);
+		visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (9.18f, -2.61f);
 
-		//this sets up the coroutine for a SECOND WIN. 
-		//basically, this speeds up the colors and makes them a bit more obnoxious. 
-		//everything here has been COMMENTED OUT until it finds it's rightful time to shine. 
 
-//		bottomLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//		bottomRight.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//
-//		secondLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//		secondRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//		thirdLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//		thirdRight.GetComponent<MeshRenderer> ().material = puperPinkMaterial;
-//
-//		fourthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//		fourthRight.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//
-//		fifthLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//		fifthRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//		heartLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//		heartRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//		StartCoroutine (HappyLightDance2 ());
+		StartCoroutine (eye1Animation ());
+
+		//****END IF EYE 1 IS CHOSEN****
 
 
 
 
 
 	}
+
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -116,11 +139,11 @@ public class Robot_Reaction : MonoBehaviour {
 
 
 
-	//the 1ST positive reaction 
+	//ANIMATION IF EYE == 0 
 	IEnumerator HappyLightDance1(){
 		while (true) {
 			yield return new WaitForSeconds (1f);
-			Debug.Log ("Level One: Purgatory");
+		
 
 			//layer 1
 			bottomLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
@@ -143,7 +166,7 @@ public class Robot_Reaction : MonoBehaviour {
 
 			//layer 2
 			yield return new WaitForSeconds (1f);
-			Debug.Log ("Level Two: Lust");
+
 			bottomLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
 			bottomRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
 
@@ -161,7 +184,7 @@ public class Robot_Reaction : MonoBehaviour {
 
 
 			//layer 3
-			Debug.Log("Level Three: Anger");
+
 			yield return new WaitForSeconds (1f);
 			bottomLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
 			bottomRight.GetComponent<MeshRenderer> ().material = blackMaterial;
@@ -179,7 +202,7 @@ public class Robot_Reaction : MonoBehaviour {
 			fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
 
 			//layer 4
-			Debug.Log("Level Four: Greed? Money People I think??");
+
 			yield return new WaitForSeconds (1f);
 			bottomLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
 			bottomRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
@@ -197,7 +220,7 @@ public class Robot_Reaction : MonoBehaviour {
 			fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
 
 			//layer 5
-			Debug.Log("Level Five: Traitors Maybe idk");
+
 			yield return new WaitForSeconds (1f);
 			bottomLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
 			bottomRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
@@ -217,117 +240,44 @@ public class Robot_Reaction : MonoBehaviour {
 			heartLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
 			heartRight.GetComponent<MeshRenderer> ().material = blackMaterial;
 		}
-
+			
 	}
 
+	//END 
 
 
 
-	//the 2ND positive reaction 
-//	IEnumerator HappyLightDance2(){
-//		while (true) {
-//			yield return new WaitForSeconds (0.3f);
-//			Debug.Log ("Level One: Purgatory");
-//
-//			//layer 1
-//			bottomLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			bottomRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			secondLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			secondRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			thirdLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			thirdRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			fourthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			fourthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			fifthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			heartLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//			heartRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			//layer 2
-//			yield return new WaitForSeconds (0.3f);
-//			Debug.Log ("Level Two: Lust");
-//			bottomLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//			bottomRight.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//
-//			secondLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			secondRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			thirdLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			thirdRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			fourthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			fourthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			fifthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//
-//			//layer 3
-//			Debug.Log("Level Three: Anger");
-//			yield return new WaitForSeconds (0.3f);
-//			bottomLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			bottomRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			secondLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//			secondRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			thirdLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			thirdRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			fourthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			fourthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			fifthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			//layer 4
-//			Debug.Log("Level Four: Greed? Money People I think??");
-//			yield return new WaitForSeconds (0.3f);
-//			bottomLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			bottomRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			secondLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//			secondRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			thirdLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//			thirdRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			fourthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			fourthRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			fifthLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//			fifthRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			//layer 5
-//			Debug.Log("Level Five: Traitors Maybe idk");
-//			yield return new WaitForSeconds (0.3f);
-//			bottomLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//			bottomRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			secondLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			secondRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			thirdLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//			thirdRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//
-//			fourthLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//			fourthRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
-//
-//			fifthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
-//			fifthRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
-//
-//			heartLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
-//			heartRight.GetComponent<MeshRenderer> ().material = blackMaterial;
-//		}
-//
-//	}
+	IEnumerator eye1Animation (){
+	
+		while (true) {
 
+			visor.GetComponent<MeshRenderer> ().material = winkyFace;
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (30.4f, 3f);
 
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (6.3f, 0f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (6.6f, 0f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (6.9f, 0f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (7.2f, 0f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (7.5f, 0f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (7.8f, 0f);
+
+		
+
+		
+		}
+	
+	}
 
 
 }
---
+++ Assets/Scripts/Second_Positives.cs
@@ -0,0 +1,262 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Second_Positives : MonoBehaviour {
+
+
+	//****the following are public objects for EYE 0****
+	//turned off for testing
+	//	public GameObject closedRight;
+	//	public GameObject openRight;
+	//public GameObject closedLeft;
+	//public GameObject openLeft;
+
+
+	//materials
+	public Material pinkMaterial;
+	public Material blackMaterial;
+	public Material superPinkMaterial;
+	public Material reallySuperPinkMaterial;
+
+	//eye1 hierarchy
+	public GameObject bottomLeft;
+	public GameObject bottomRight;
+
+	public GameObject secondLeft;
+	public GameObject secondRight;
+
+	public GameObject thirdLeft;
+	public GameObject thirdRight;
+
+	public GameObject fourthLeft;
+	public GameObject fourthRight;
+
+	public GameObject fifthLeft;
+	public GameObject fifthRight;
+
+	public GameObject heartLeft;
+	public GameObject heartRight;
+
+	//****end objects for EYE 0.****
+
+
+	//****THE FOLLOWING ARE PUBLIC OBJECTS FOR EYE 1 (visor)****
+
+	public GameObject visor;
+	public Material goodReaction;
+	public Material superWinkyFace;
+
+	//****END OBJECTS FOR EYE 1.
+
+
+	// Use this for initialization
+	void Start () {
+
+//opens eyes if closed before. also commented out for testing. 
+//		foreach (MeshRenderer closedLeftRenderer in closedLeft.GetComponentsInChildren<MeshRenderer>()) {
+//			closedLeftRenderer.enabled = false;
+//		}
+//
+//		foreach (MeshRenderer openLeftRenderer in openLeft.GetComponentsInChildren<MeshRenderer>()) {
+//			openLeftRenderer.enabled = true;
+//		}
+//
+//		//this replaces the closed right eye with an open right eye. 
+//		foreach (MeshRenderer openRightRenderer in openRight.GetComponentsInChildren<MeshRenderer>()) {
+//			openRightRenderer.enabled = true;
+//		}
+//
+//		foreach (MeshRenderer closedRightRenderer in closedRight.GetComponentsInChildren<MeshRenderer>()) {
+//			closedRightRenderer.enabled = false;
+//		}
+
+	
+		//this sets up the coroutine for a SECOND WIN. 
+		//basically, this speeds up the colors and makes them a bit more obnoxious. 
+		//everything here has been COMMENTED OUT until it finds it's rightful time to shine. 
+
+				bottomLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				bottomRight.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+		
+				secondLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				secondRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+		
+				thirdLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
+				thirdRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+		
+				fourthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				fourthRight.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+		
+				fifthLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				fifthRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+		
+				heartLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
+				heartRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+		
+				StartCoroutine (HappyLightDance2 ());
+
+
+
+
+		//****IF EYE 1 IS CHOSEN****
+
+		visor.GetComponent<MeshRenderer> ().material = goodReaction;
+		visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (0.02f, -0.12f);
+		visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (9.18f, -2.61f);
+		StartCoroutine (eye1Animation ());
+
+		//****END IF EYE 1 IS CHOSEN****
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//the 2ND positive reaction 
+		IEnumerator HappyLightDance2(){
+			while (true) {
+				yield return new WaitForSeconds (0.3f);
+				Debug.Log ("Level One: Purgatory");
+	
+				//layer 1
+				bottomLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				bottomRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				secondLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				secondRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				thirdLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				thirdRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				fourthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				fourthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				fifthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				heartLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
+				heartRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				//layer 2
+				yield return new WaitForSeconds (0.3f);
+				Debug.Log ("Level Two: Lust");
+				bottomLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				bottomRight.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+	
+				secondLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				secondRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				thirdLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				thirdRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				fourthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				fourthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				fifthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+	
+				//layer 3
+				Debug.Log("Level Three: Anger");
+				yield return new WaitForSeconds (0.3f);
+				bottomLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				bottomRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				secondLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				secondRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				thirdLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				thirdRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				fourthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				fourthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				fifthLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				fifthRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				//layer 4
+				Debug.Log("Level Four: Greed? Money People I think??");
+				yield return new WaitForSeconds (0.3f);
+				bottomLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				bottomRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				secondLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
+				secondRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				thirdLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				thirdRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				fourthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				fourthRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				fifthLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
+				fifthRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				//layer 5
+				Debug.Log("Level Five: Traitors Maybe idk");
+				yield return new WaitForSeconds (0.3f);
+				bottomLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				bottomRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				secondLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				secondRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				thirdLeft.GetComponent<MeshRenderer> ().material = pinkMaterial;
+				thirdRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+	
+				fourthLeft.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+				fourthRight.GetComponent<MeshRenderer> ().material = superPinkMaterial;
+	
+				fifthLeft.GetComponent<MeshRenderer> ().material = reallySuperPinkMaterial;
+				fifthRight.GetComponent<MeshRenderer> ().material = pinkMaterial;
+	
+				heartLeft.GetComponent<MeshRenderer> ().material = blackMaterial;
+				heartRight.GetComponent<MeshRenderer> ().material = blackMaterial;
+			}
+	
+		}
+
+
+	//FOR EYE 1 ANIMATIONS
+	IEnumerator eye1Animation (){
+
+		while (true) {
+
+			visor.GetComponent<MeshRenderer> ().material = superWinkyFace;
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (6.3f, 0f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (30.4f, 3f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (6.6f, 0f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (35.84f, 4.92f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (6.9f, 0f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (54.24f, 8.1f);
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (7.2f, 0f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (106.1f, 14.2f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (7.5f, 0f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (199.6f, 34.6f);
+
+			yield return new WaitForSeconds (0.3f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2 (7.8f, 0f);
+			visor.GetComponent<MeshRenderer> ().material.mainTextureScale = new Vector2 (411.2f, 234.1f);
+
+
+
+
+		}
+
+	}
+
+
+
+}
